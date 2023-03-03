3a10e00fc23d7b958722cfc7fd33cae58cf1f2fc
Trisha <trishaposey@gmail.com>
2023-03-02 20:12:45 -0600

    hopefully final push

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// TODO: add code here
+window.addEventListener("load", function() {
+    const container = document.getElementById("container")
+
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+        for (let i=0; i < data.length; i++) {
+        let astronaut = data[i];
+        container.innerHTML += `
+        <div class="astronaut">
+            <div class="bio">
+                <h3>${astronaut.firstName} ${astronaut.lastName} </h3>
+                <ul>
+                    <li>${astronaut.hoursInSpace}</li>
+                    <li>${astronaut.active}</li>
+                    <li>${astronaut.skills.join(", ")}</li>
+                </ul>
+                </div>
+                <img class="avatar" src="${astronaut.picture}"></img>
+                </div> `
+        }
+      });
+    });
+})
